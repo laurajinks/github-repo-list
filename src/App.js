88f5024fa827ab9618d6e69b744251bc3fd15be2
Laura <laura.jinks@gmail.com>
2020-11-14 14:48:04 -0600

    feat(app): add tailwind/react-router-dom/component file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
 import './App.css';
+import './tailwind.output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
